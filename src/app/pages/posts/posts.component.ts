79ef54333030a9b15afddaf284550e493ce0bf7b
Leela <leela.narsimha@gmail.com>
2021-10-11 09:20:14 +0530

    Format posts data and display the data in ui

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { IPost } from 'src/app/models/IPost';
 import { PostService } from 'src/app/services/post.service';
 
 @Component({
@@ -6,12 +8,18 @@ import { PostService } from 'src/app/services/post.service';
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss'],
 })
-export class PostsComponent implements OnInit {
+export class PostsComponent implements OnInit, OnDestroy {
+  posts: IPost[] = [];
+  postsSubscription!: Subscription;
   constructor(private postService: PostService) {}
 
   ngOnInit(): void {
-    this.postService.getPosts().subscribe((data) => {
-      console.log(data);
+    this.postsSubscription = this.postService.getPosts().subscribe((data) => {
+      this.posts = data;
     });
+  }
+
+  ngOnDestroy() {
+    this.postsSubscription && this.postsSubscription.unsubscribe();
   }
 }
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 import { IPost } from '../models/IPost';
 
 @Injectable({
@@ -9,8 +10,18 @@ export class PostService {
   constructor(private http: HttpClient) {}
 
   getPosts() {
-    return this.http.get<{ [id: string]: IPost }>(
-      `https://rxjs-posts-default-rtdb.firebaseio.com/posts.json`
-    );
+    return this.http
+      .get<{ [id: string]: IPost }>(
+        `https://rxjs-posts-default-rtdb.firebaseio.com/posts.json`
+      )
+      .pipe(
+        map((posts) => {
+          let postsData: IPost[] = [];
+          for (let id in posts) {
+            postsData.push({ ...posts[id], id });
+          }
+          return postsData;
+        })
+      );
   }
 }
