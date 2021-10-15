e3a3f986b3bb7ce7bfccd565bab60032048459d7
Leela <leela.narsimha@gmail.com>
2021-10-15 13:28:38 +0530

    filtered posts in the category

--- src/app/pages/declarative-posts/declarative-posts.component.ts
+++ src/app/pages/declarative-posts/declarative-posts.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { map } from 'rxjs';
+import { DeclarativeCategoryService } from 'src/app/services/DeclarativeCategory.service';
 import { DeclarativePostService } from 'src/app/services/DeclarativePost.service';
 
 @Component({
@@ -8,7 +10,26 @@ import { DeclarativePostService } from 'src/app/services/DeclarativePost.service
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DeclarativePostsComponent {
+  selectedCategoryId = '';
   posts$ = this.postService.postsWithCategory$;
+  categories$ = this.categoryService.categories$;
 
-  constructor(private postService: DeclarativePostService) {}
+  filteredPosts$ = this.posts$.pipe(
+    map((posts) => {
+      return posts.filter((post) =>
+        this.selectedCategoryId
+          ? post.categoryId === this.selectedCategoryId
+          : true
+      );
+    })
+  );
+
+  constructor(
+    private postService: DeclarativePostService,
+    private categoryService: DeclarativeCategoryService
+  ) {}
+  onCategoryChange(event: Event) {
+    let selectedCategoryId = (event.target as HTMLSelectElement).value;
+    this.selectedCategoryId = selectedCategoryId;
+  }
 }
