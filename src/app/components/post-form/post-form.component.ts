6b48e83b2ba640d130751e8bdd3005af811edf44
Leela <leela.narsimha@gmail.com>
2021-10-28 22:06:06 +0530

    selected post details with route param

--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { combineLatest, map, tap } from 'rxjs';
 import { DeclarativeCategoryService } from 'src/app/services/DeclarativeCategory.service';
+import { DeclarativePostService } from 'src/app/services/DeclarativePost.service';
 
 @Component({
   selector: 'app-post-form',
@@ -7,9 +11,44 @@ import { DeclarativeCategoryService } from 'src/app/services/DeclarativeCategory
   styleUrls: ['./post-form.component.scss'],
 })
 export class PostFormComponent implements OnInit {
+  postForm = new FormGroup({
+    title: new FormControl(''),
+    description: new FormControl(''),
+    categoryId: new FormControl(''),
+  });
+
+  selectedPostId = this.route.paramMap.pipe(
+    map((paramMap) => {
+      let id = paramMap.get('id');
+      this.postService.selectPost(id + '');
+      return id;
+    })
+  );
+
+  post$ = this.postService.post$.pipe(
+    tap((post) => {
+      post &&
+        this.postForm.setValue({
+          title: post?.title,
+          description: post?.description,
+          categoryId: post?.categoryId,
+        });
+    })
+  );
+
   categories$ = this.categoryService.categories$;
 
-  constructor(private categoryService: DeclarativeCategoryService) {}
+  vm$ = combineLatest([this.selectedPostId, this.post$]);
+
+  constructor(
+    private categoryService: DeclarativeCategoryService,
+    private route: ActivatedRoute,
+    private postService: DeclarativePostService
+  ) {}
 
   ngOnInit(): void {}
+
+  onPostSubmit() {
+    console.log(this.postForm.value);
+  }
 }
